02bd198f789e7d9a338242c89bd036f11c2a164c
YeseniaCM <yeseniacamilla@hotmail.com>
2024-03-26 16:49:47 +0100

    :sparkles: adding toggle funtion on main

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './scss/style.scss'
 import { printHomepage } from './homePage';
 import { printimagesBoxes } from './imageBoxes';
+import { menuToggle } from './toggleMenu';
 
 export let container = document.createElement('div');
 container.classList.add('container');
