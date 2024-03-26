23f36ffaaf2075bcbb34183facc7813cfecad916
YeseniaCM <yeseniacamilla@hotmail.com>
2024-03-26 16:29:36 +0100

    :lipstick:

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './css/style.css'
 import { printimagesBoxes } from './imageBoxes';
 
 export let container = document.createElement('div');
-container.classList.add('conatiner');
+container.classList.add('container');
 printimagesBoxes()
 
 
