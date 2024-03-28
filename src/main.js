969f5249174817519d31fc16e4c4e75768782cef
YeseniaCM <yeseniacamilla@hotmail.com>
2024-03-28 19:58:50 +0100

    -

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './sscss/style.sscss'
+import './scss/style.scss'
 import { printHomepage } from './homePage';
 import { printimagesBoxes } from './imageBoxes';
 import { menuToggle } from './toggleMenu';
